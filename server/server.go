c5cf7cd2f3fdba3c51e768764ec55742ee1c71e7
MacAdmins @ecuad.ca <5473773+ca-ecuad-macadmins@users.noreply.github.com>
2023-04-06 13:25:30 -0700

    MicroMDM Custumizations due to errors 500 from SCEP cert validation

--- server/server.go
+++ server/server.go
@@ -19,7 +19,7 @@ import (
 	configbuiltin "github.com/micromdm/micromdm/platform/config/builtin"
 	"github.com/micromdm/micromdm/platform/dep/sync"
 	syncbuiltin "github.com/micromdm/micromdm/platform/dep/sync/builtin"
-	"github.com/micromdm/micromdm/platform/device"
+// 	"github.com/micromdm/micromdm/platform/device"
 	devicebuiltin "github.com/micromdm/micromdm/platform/device/builtin"
 	"github.com/micromdm/micromdm/platform/profile"
 	profilebuiltin "github.com/micromdm/micromdm/platform/profile/builtin"
@@ -202,11 +202,11 @@ func (c *Server) setupCommandQueue(logger log.Logger) error {
 		mdmService = svc
 		mdmService = block.RemoveMiddleware(c.RemoveDB)(mdmService)
 
-		udidauthLogger := log.With(logger, "component", "udidcertauth")
-		mdmService = device.UDIDCertAuthMiddleware(devDB, udidauthLogger, c.UDIDCertAuthWarnOnly)(mdmService)
+// 		udidauthLogger := log.With(logger, "component", "udidcertauth")
+// 		mdmService = device.UDIDCertAuthMiddleware(devDB, udidauthLogger, c.UDIDCertAuthWarnOnly)(mdmService)
 
-		verifycertLogger := log.With(logger, "component", "verifycert")
-		mdmService = VerifyCertificateMiddleware(c.ValidateSCEPIssuer, c.ValidateSCEPExpiration, c.SCEPDepot, verifycertLogger)(mdmService)
+// 		verifycertLogger := log.With(logger, "component", "verifycert")
+// 		mdmService = VerifyCertificateMiddleware(c.ValidateSCEPIssuer, c.ValidateSCEPExpiration, c.SCEPDepot, verifycertLogger)(mdmService)
 	}
 	c.MDMService = mdmService
 
